5f6a1f086b0113eb581d31888560fe4e3be22616
Oguzhan <126396389+oguzhanfak@users.noreply.github.com>
2024-02-07 00:27:15 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+const container =document.querySelector(".container")
+const selectMovie= document.querySelector("#selectMovie")
+const count= document.querySelector("#count")
+const amount= document.querySelector("#amount")
+const seats = Array.from(document.querySelectorAll(".seat"))
+const buyButton =document.querySelector("#buyButton")
+
+runEventListeners()
+
+function runEventListeners(){
+    container.addEventListener("click",select)
+    selectMovie.addEventListener("change",changeMovie)
+    document.addEventListener("DOMContentLoaded",runPageLoaded)
+    buyButton.addEventListener("click",buyTicket)
+}
+function runPageLoaded(){
+   const selectedSeatsIndex= Storagex.getSelectedSeatsFromStorage()
+   const fullSeatsIndex= Storagex.getFullSeatsFromStorage()
+
+   seats.forEach((seat, index)=>{
+    if(selectedSeatsIndex.includes(index)){
+        seat.classList.add("selected")
+    }
+   })
+   seats.forEach((seat, index)=>{
+    if(fullSeatsIndex.includes(index)){
+        seat.classList.add("full")
+    }
+   })
+
+   selectMovie.selectedIndex=Storagex.getSelectedMovieIndexFromStorage()
+   calculate()
+}
+function buyTicket(){
+if(confirm("satın almak istiyor musunuz?")){
+    const selectedSeats= getSelectedSeats()
+ const selectedSeatsIndex = getSelectedSeatsIndex()
+   selectedSeats.forEach(seat=>{
+    seat.classList.remove("selected")
+    seat.classList.add("full")
+   })
+   Storagex.addFullSeatToStorage(selectedSeatsIndex)
+   Storagex.addSelectedSeatToStorage(getSelectedSeatsIndex())
+}
+}
+function select(e){
+    const selectedElement= e.target.parentElement
+    if(selectedElement.classList.contains("seat") && !selectedElement.classList.contains(".full")){
+        selectedElement.classList.toggle("selected")
+        calculate()
+        saveSelectedSeatsIndexToStorage()
+        saveSelectedMovieIndexToStorage()
+    } 
+}
+function changeMovie(){
+    calculate()
+    saveSelectedMovieIndexToStorage()
+}
+function getSelectedSeats(){
+    const selectedList = [...container.querySelectorAll(".selected")]
+    return selectedList
+}
+function getSelectedSeatsIndex(){
+    const selectedList= getSelectedSeats()
+    const selectedSeatsIndex=selectedList.map((seat)=>{
+        return seats.indexOf(seat)
+    }) 
+    return selectedSeatsIndex
+}
+function saveSelectedSeatsIndexToStorage(){
+   const selectedSeatsIndex= getSelectedSeatsIndex()
+    Storagex.addSelectedSeatToStorage(selectedSeatsIndex)
+}
+function saveSelectedMovieIndexToStorage(){
+    const selectedMovieIndex= selectedMovieToStorage(selectedMovieIndex)
+    Storagex.addSelectedMovieToStorage(selectedMovieIndex)
+}
+function calculate(){
+const selectedSeatsCount = getSelectedSeats().length
+const price = selectMovie.options[selectMovie.selectedIndex].value
+count.textContent=selectedSeatsCount
+amount.textContent=selectedSeatsCount*price
+}
